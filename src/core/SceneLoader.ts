14c5d623987f31a67a0640219efb64bacf87dd13
inc0der <inc0der.dev@gmail.com>
2022-04-17 23:29:28 -0300

    Add has and get methods

--- src/core/SceneLoader.ts
+++ src/core/SceneLoader.ts
@@ -2,39 +2,48 @@ import { utils } from 'pixi.js';
 import type Scene from './Scene';
 
 export default class SceneLoader extends utils.EventEmitter {
-  private scenes: Map<string, IScene> = new Map();
+  private scenes: Map<string, Scene> = new Map();
 
   private currentScene: Scene;
 
   private lastScene: Scene;
 
-    add(scene: Scene, name?: string, ) {
-      const name = name || scene.key;
-      if (this.scenes.has(name)) {
-        throw new Error(`Scene with key ${name} already exists`);
-      }
-        scene.name = name;
-        this.scenes.set(name, scene);
-        this.emit('sceneAdd', scene);
-    }
+  public has(scene: string | Scene) {
+    const name = typeof scene === 'string' ? scene : scene.name;
+    return this.scenes.has(name);
+  }
 
-    remove(scene: string | Scene) {
-      const name = typeof scene === 'string' ? scene : scene.name;
-      if (this.scenes.has(name)) {
-        this.scenes.delete(scene);
-      }
-    }
+  public get(name: string) {
+    return this.scenes.get(name);
+  }
 
-    pop() {
-      const tempLastScene = this.currentScene;
-        this.currentScene = this.lastScene;
-        this.lastScene = tempLastScene;
-        this.emit('sceneChange', this.currentScene);
+  public add(scene: Scene, name?: string) {
+    const key = name || scene.key;
+    if (this.scenes.has(key)) {
+      throw new Error(`Scene with key ${key} already exists`);
     }
+    scene.setName(key);
+    this.scenes.set(key, scene);
+    this.emit('sceneAdd', scene);
+  }
 
-    change(scene: Scene) {
-        this.lastScene = this.currentScene;
-        this.currentScene = scene;
-        this.emit('sceneChange', scene);
+  public remove(scene: string | Scene) {
+    const name = typeof scene === 'string' ? scene : scene.name;
+    if (this.scenes.has(name)) {
+      this.scenes.delete(scene);
     }
+  }
+
+  public pop() {
+    const tempLastScene = this.currentScene;
+    this.currentScene = this.lastScene;
+    this.lastScene = tempLastScene;
+    this.emit('sceneChange', this.currentScene);
+  }
+
+  public change(scene: Scene) {
+    this.lastScene = this.currentScene;
+    this.currentScene = scene;
+    this.emit('sceneChange', scene);
+  }
 }
