f8e32911a31ef8ccafaa9e0a254f057dd41fbfa1
inc0der <inc0der.dev@gmail.com>
2022-04-24 19:33:32 -0300

    Update index files to export properly for modular bases imports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export * from './core/'
+import * as ldtkParser from './utils/ldtkParser';
+import Entity from './entities/Entity';
+import Game from './Game';
+
+export * from './core/';
+
+export { Game, Entity, ldtkParser };
